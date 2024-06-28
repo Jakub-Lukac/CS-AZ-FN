603a4693be0701a68ce1aa7d19324382578dd9ed
Jakub-Lukac <jakub.lukac81@gmail.com>
2024-06-28 11:07:42 +0200

    path adjusted

--- CS-AZ-FN-Functions/Helpers/Code.cs
+++ CS-AZ-FN-Functions/Helpers/Code.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using Azure.Core;
+using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +13,32 @@ namespace CS_AZ_FN_Functions.Helpers
 {
     public class Code
     {
+        public GraphServiceClient GetAuthenticatedGraphClient(string tenantId, string clientId, string clientSecret)
+        {
+            var scopes = new[] { "https://graph.microsoft.com/.default" };
 
+            var options = new TokenCredentialOptions
+            {
+                AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
+            };
+
+            var clientSecretCredential = new ClientSecretCredential(tenantId, clientId, clientSecret, options);
+
+            return new(clientSecretCredential, scopes);
+        }
+
+        public Task<User> GetUser(GraphServiceClient graphServiceClient, string userId)
+        {
+            try
+            {
+                var result = graphServiceClient.Users[userId].GetAsync();
+                return result;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return null;
+            }
+        }
     }
 }
---
+++ CS-AZ-FN-Functions/Helpers/Model.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_AZ_FN_Functions.Helpers
+{
+    public class Model
+    {
+        public string tenantId { get; set; }
+        public string appId { get; set; }
+        public string appSecret { get; set; }
+        public string userId { get; set; }
+
+        public string groupId { get; set; }
+        public string eventHubConnectionString { get; set; }
+        public string eventHubName { get; set; }
+
+        public Model()
+        {
+            tenantId = Environment.GetEnvironmentVariable("CONF_TENANT_ID");
+            appId = Environment.GetEnvironmentVariable("CONF_APP_ID");
+            appSecret = Environment.GetEnvironmentVariable("CONF_APP_SECRET");
+
+            eventHubConnectionString = Environment.GetEnvironmentVariable("CONF_EVENTHUB_CONNECTION_STRING");
+            eventHubName = Environment.GetEnvironmentVariable("CONF_EVENTHUB_NAME");
+        }
+    }
+}
